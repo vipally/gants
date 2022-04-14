ac1ccde56354bfbc0b258c1b686cd383fb6e80c3
Ally Dale <vipally@gmail.com>
2022-04-14 10:25:29 +0800

    update

---
+++ examples/runner_pool.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"sync"
+)
+
+type runner struct {
+	arg int64
+}
+
+func (r *runner) clean() *runner {
+	return r
+}
+
+func (r *runner) run() {
+
+}
+
+type runnerPool struct {
+	lock sync.Mutex
+
+	pool  []*runner
+	idGen uint64
+}
+
+func (p *runnerPool) Acquire() *runner {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	var r *runner
+	if size := len(p.pool); size > 0 {
+		r = p.pool[size-1]
+		p.pool = p.pool[:size-1]
+	} else {
+		r = &runner{}
+	}
+
+	return r
+}
+
+func (p *runnerPool) Recycle(t *runner) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	p.pool = append(p.pool, t.clean())
+}
